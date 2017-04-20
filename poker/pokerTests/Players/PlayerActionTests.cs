b87825dcf4762be9b593fde7c5d65786a71d0461
moshekar <moshe_kar0@walla.co.il>
2017-04-20 15:42:45 +0300

    register done+unit tests

--- poker/pokerTests/Players/PlayerActionTests.cs
+++ poker/pokerTests/Players/PlayerActionTests.cs
@@ -24,5 +24,33 @@ namespace poker.Players.Tests
             ans = PlayerAction.Login("ronen", "1234", playersData);
             Assert.AreEqual(ans, player);
         }
+        [TestMethod()]
+        public void RegisterTest()
+        {
+            Player player = new Player(10, "ronen", "1234", "rons@gmail.com", leaguesData.GetDefalutLeague());
+            int users = playersData.GetAllPlayers().Count;
+            bool ans = PlayerAction.Register(player, playersData);
+            Assert.IsTrue(ans);
+            Assert.IsTrue(users == playersData.GetAllPlayers().Count - 1);
+            users = playersData.GetAllPlayers().Count;
+            player = new Player(11, "rohama", "1234", "dfdf", leaguesData.GetDefalutLeague());
+            ans = PlayerAction.Register(player, playersData);
+            Assert.IsFalse(ans);
+            Assert.IsTrue(users == playersData.GetAllPlayers().Count);
+            users = playersData.GetAllPlayers().Count;
+            player.SetEmail("dfdf@gmail.com");
+            ans = PlayerAction.Register(player, playersData);
+            Assert.IsTrue(ans);
+            Assert.IsTrue(users == playersData.GetAllPlayers().Count - 1);
+            users = playersData.GetAllPlayers().Count;
+            ans = PlayerAction.Register(player, playersData);
+            Assert.IsFalse(ans);
+            Assert.IsTrue(users == playersData.GetAllPlayers().Count);
+            users = playersData.GetAllPlayers().Count;
+            player = new Player(11, "ronen", "123456", "rons@gmail.com", leaguesData.GetDefalutLeague());
+            ans = PlayerAction.Register(player, playersData);
+            Assert.IsFalse(ans);
+            Assert.IsTrue(users == playersData.GetAllPlayers().Count);
+        }
     }
 }
