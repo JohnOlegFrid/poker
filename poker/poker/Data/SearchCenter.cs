1d540446d1a5a936de7c40c94b3ca36fd4bca5c4
olej <oleg333@gmail.com>
2017-04-21 19:01:27 +0300

    new Class - SearchCenter purpose - concentrate all posible user searches in one place. new method in SearchCenter : SearchGameByPlayerUserName - arguments - String : players userName return List<IGame> of all active games the player with userName existes in.

---
+++ poker/poker/Data/SearchCenter.cs
@@ -0,0 +1,41 @@
+﻿using poker.Center;
+using poker.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poker.Data
+{
+    class SearchCenter
+    {
+        private PlayersByList playersData;
+        private LeaguesByList leaguesData;
+
+        public SearchCenter (PlayersByList newPlayerData,LeaguesByList newLeagueData)
+        {
+            this.playersData = newPlayerData;
+            this.leaguesData = newLeagueData;
+        }
+
+        public List<IGame> SearchGamesByPlayerUserName(String userName)
+        {
+            List<IGame> ans = new List<IGame>();
+            List<Room> listOfRoomsWithActiveGames = new List<Room>();
+            Player searchedPlayer = playersData.FindPlayerByUsername(userName);
+            if (searchedPlayer == default(Player)) return null; // the player we search doesn't exist.
+            else
+            {
+                listOfRoomsWithActiveGames=searchedPlayer.League.GetAllActiveGames();
+                foreach(Room r in listOfRoomsWithActiveGames)
+                {
+                    if (r.IsPlayerActiveInRoom(searchedPlayer))
+                        ans.Add(r.Game);
+                }
+            }
+            return ans;
+
+        }
+    }
+}
