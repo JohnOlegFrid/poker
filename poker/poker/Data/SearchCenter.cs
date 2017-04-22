a083dedeee99d5ce6f83045923b48c53515d9931
olej <oleg333@gmail.com>
2017-04-22 22:24:56 +0300

    SearchCenter Class changed to public.

--- poker/poker/Data/SearchCenter.cs
+++ poker/poker/Data/SearchCenter.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace poker.Data
 {
-    class SearchCenter
+    public class SearchCenter
     {
         private IPlayersData playersData;
         private ILeaguesData leaguesData;
