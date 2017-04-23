8ac23160c3eb4895667119a1f7e00c79361881d6
moshekar <moshe_kar0@walla.co.il>
2017-04-23 13:47:54 +0300

    refactored a little bit

--- poker/poker/PokerGame/TexasGame.cs
+++ poker/poker/PokerGame/TexasGame.cs
@@ -12,7 +12,7 @@ namespace poker.PokerGame
 {
     public class TexasGame : IGame
     {
-        private GamePlayer[] playersInGame;
+        private GamePlayer[] chairsInGame;
         private int currentPlayers;
         private List<Player> spectators;
         private bool active;
@@ -30,7 +30,7 @@ namespace poker.PokerGame
         public TexasGame(GamePreferences gp)
         {
             this.gamePreferences = gp;
-            playersInGame = new GamePlayer[this.gamePreferences.MaxPlayers];
+            chairsInGame = new GamePlayer[this.gamePreferences.MaxPlayers];
             spectators = new List<Player>();
             active = false;
             gameLog = new List<string>();
@@ -53,13 +53,13 @@ namespace poker.PokerGame
             }
         }
 
-        public List<int> AskToJoin() //the method returns list of free chairs , why its AskToJoin? doesn't clear enough.
+        public List<int> getFreeChairs() //the method returns list of free chairs , why its AskToJoin? doesn't clear enough.
         {
             List<int> ans = new List<int>();
             if (active)
             {
                 for (int i = 0; i < gamePreferences.MaxPlayers; i++)
-                    if (playersInGame[i] == null)
+                    if (chairsInGame[i] == null)
                         ans.Add(i);
             }
             return ans;
@@ -69,16 +69,16 @@ namespace poker.PokerGame
         {
             for(int i=0; i<gamePreferences.MaxPlayers;i++)
             {
-                if ((playersInGame[i] != null) && (playersInGame[i].Equals(p))) //a player can't join a game twice.
+                if ((chairsInGame[i] != null) && (chairsInGame[i].Equals(p))) //a player can't join a game twice.
                     return false;
             }
-            if (playersInGame[chair] != null)
+            if (chairsInGame[chair] != null)
                 return false;
             if (amount < gamePreferences.MinBuyIn || amount > gamePreferences.MaxBuyIn)
                 return false;
             if (amount > p.Money)
                 return false;
-            playersInGame[chair] = p;
+            chairsInGame[chair] = p;
             p.ChairNum = chair;
             gameLog.Add(p.Player.Username + " joined the game.");
             currentPlayers++;
@@ -134,7 +134,7 @@ namespace poker.PokerGame
         {
             if (activePlayer == null)
                 return activePlayer;
-            return playersInGame[this.activePlayer.ChairNum];
+            return chairsInGame[this.activePlayer.ChairNum];
         }
         
         public void NextTurn()
@@ -212,8 +212,8 @@ namespace poker.PokerGame
             int chair = activePlayer.ChairNum;
             for(int i=1; i<gamePreferences.MaxPlayers - chair; i++)
             {
-                if (chair+1 < gamePreferences.MaxPlayers && playersInGame[chair + i] != null && !playersInGame[chair + i].IsFold())
-                    return playersInGame[chair + i];
+                if (chair+1 < gamePreferences.MaxPlayers && chairsInGame[chair + i] != null && !chairsInGame[chair + i].IsFold())
+                    return chairsInGame[chair + i];
 
             }
             return null;
@@ -222,10 +222,10 @@ namespace poker.PokerGame
         // return null if no more active players
         public GamePlayer GetFirstPlayer()
         {
-            for(int i = 0; i < this.playersInGame.Length; i++)
+            for(int i = 0; i < this.chairsInGame.Length; i++)
             {
-                if (playersInGame[i] != null && !playersInGame[i].IsFold())
-                    return playersInGame[i];
+                if (chairsInGame[i] != null && !chairsInGame[i].IsFold())
+                    return chairsInGame[i];
             }
             return null;
         }
@@ -233,8 +233,8 @@ namespace poker.PokerGame
         public List<Player> GetListActivePlayers()
         {
             List<Player> ans = new List<Player>();
-            if (playersInGame.Length == 0 || !Active) return null; // no active players for that game
-            foreach(GamePlayer p in playersInGame)
+            if (chairsInGame.Length == 0 || !Active) return null; // no active players for that game
+            foreach(GamePlayer p in chairsInGame)
             {
                 if(p!=null)
                     ans.Add(p.Player);
@@ -247,7 +247,7 @@ namespace poker.PokerGame
             if (!(obj is TexasGame))
                 return false;
             TexasGame tg = (TexasGame)obj;
-            if (tg.playersInGame != playersInGame)
+            if (tg.chairsInGame != chairsInGame)
                 return false;
             return true;
         }
