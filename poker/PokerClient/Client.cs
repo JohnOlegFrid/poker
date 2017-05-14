498d5e4f0cbb168289dafbc8b19bb2877fb18224
YakirZana <yaqir11@gmail.com>
2017-05-14 17:09:23 +0300

    init client basic into master

---
+++ poker/PokerClient/Client.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPoker
+{
+    class Client
+    {
+        private TcpClient client;
+
+        private StreamReader sReader;
+        private StreamWriter sWriter;
+
+        private Boolean isConnected;
+
+        public Client(String ipAddress, int portNum)
+        {
+            client = new TcpClient();
+            client.Connect(ipAddress, portNum);
+            isConnected = true;
+
+
+            sReader = new StreamReader(client.GetStream(), Encoding.ASCII);
+            sWriter = new StreamWriter(client.GetStream(), Encoding.ASCII);
+        }
+
+        public string SendMessage(Object obj)
+        {
+            sWriter.WriteLine(JsonConvert.SerializeObject(obj));
+            sWriter.Flush();
+
+            // if you want to receive anything
+            return sReader.ReadLine();
+        }
+
+        public void CloseConnection()
+        {
+            isConnected = false;
+            sReader.Close();
+            sWriter.Close();
+            client.Close();
+        }
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+    }
+}
---
+++ poker/PokerClient/Command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPoker
+{
+    class Command
+    {
+        public string commandName;
+        public string[] args;
+
+        public Command(string commandName, string[] args)
+        {
+            this.commandName = commandName;
+            this.args = args;
+        }
+    }
+}
