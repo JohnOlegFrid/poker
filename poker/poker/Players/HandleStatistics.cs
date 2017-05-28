24b3d26b47b0e41c685e9405a34930bf6c0a71b3
moshekar <moshe_kar0@walla.co.il>
2017-05-28 17:32:59 +0300

    added getTop func

---
+++ poker/poker/Players/HandleStatistics.cs
@@ -0,0 +1,36 @@
+﻿using poker.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poker.Players
+{
+    public static class HandleStatistics
+    {
+        public static List<Player> GetTop(string type, int number, IPlayersData data)
+        {
+            List<Player> top;
+            if (data.GetAllPlayers().Count <= number)
+                top = data.GetAllPlayers();
+            else
+            {
+                top = new List<Player>();
+                switch (type)
+                {
+                    case "Gross profit":
+                        top=data.GetAllPlayers().OrderByDescending(o => o.Total_gross_profit).Take(number).ToList();
+                        break;
+                    case "Highest gain":
+                        top = data.GetAllPlayers().OrderByDescending(o => o.Best_win).Take(number).ToList();
+                        break;
+                    case "Number of games":
+                        top = data.GetAllPlayers().OrderByDescending(o => o.Num_of_games).Take(number).ToList();
+                        break;
+                }
+            }
+            return top;
+        }
+    }
+}
---
+++ poker/pokerTests/Players/HandleStatisticsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using poker.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poker.Players.Tests
+{
+    [TestClass()]
+    public class HandleStatisticsTests
+    {
+        [TestMethod()]
+        public void GetTopTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
