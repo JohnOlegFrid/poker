db25279a9e210dc79428ba5278b1f24b0da161fc
Yakir Zana <yaqir11@gmail.com>
2017-06-27 01:26:26 +0300

    remove unessery GameCenter! start working recovery

--- poker/poker/Logs/RecoveryGame.cs
+++ poker/poker/Logs/RecoveryGame.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using poker.PokerGame;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace poker.Logs
 {
     class RecoveryGame
     {
+        public static void CreateBackupForGame(int roomId, TexasGame game)
+        {
+            
+        }
     }
 }
