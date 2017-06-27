862519520c2141fd3b0ca088fb1e5c9765f2b3e0
Yakir Zana <yaqir11@gmail.com>
2017-06-27 21:04:58 +0300

    fix bug

--- poker/poker/ServiceLayer/CenterService.cs
+++ poker/poker/ServiceLayer/CenterService.cs
@@ -20,7 +20,7 @@ namespace poker.ServiceLayer
         public string GetAllRoomsToPlay(string username)
         {
             Player player = service.PlayersData.FindPlayerByUsername(username);
-            if(player.Num_of_games > 10)
+            if(player.Num_of_games < 10)
                 return service.CreateJson(service.RoomsData.GetAllRooms());
             League l = service.LeaguesData.FindLeagueById(player.LeagueId);
             return service.CreateJson(l.Rooms);
