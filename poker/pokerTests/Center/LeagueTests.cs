305f8ee6728927b58156c824e893343613d7e867
YakirZana <yaqir11@gmail.com>
2017-06-27 19:24:23 +0300

    add units

--- poker/pokerTests/Center/LeagueTests.cs
+++ poker/pokerTests/Center/LeagueTests.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using poker.PokerGame;
 using poker.Data;
 using poker.ServiceLayer;
+using pokerTests;
 
 namespace poker.Center.Tests
 {
     [TestClass()]
     public class LeagueTests
     {
+        [TestMethod()]
+        public void AddLeagueTest()
+        {
+            ProgramList.InitData();
+            League leauge = new League(100, "leauge");
+            Program.leaguesData.AddLeague(leauge);
+            Assert.IsNotNull(Program.leaguesData.FindLeagueById(100));
+        }
+
+        [TestMethod()]
+        public void RemoveLeagueTest()
+        {
+            ProgramList.InitData();
+            League leauge = new League(100, "leauge");
+            Program.leaguesData.AddLeague(leauge);
+            Program.leaguesData.DeleteLeague(leauge);
+            Assert.IsNull(Program.leaguesData.FindLeagueById(100));
+        }
+
 
     }
 }
